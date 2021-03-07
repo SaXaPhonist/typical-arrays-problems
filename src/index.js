00dc3eeefe520ba63559b855ee9ee13f93cb49e9
Aliaksandr <dfdf1534@gmail.com>
2021-03-08 00:03:35 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined){return 0}else if(array.length === 0){return 0}
+    else{
+  array.sort((a,b) => (a-b));
+  return array[0];}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined){return 0}else if(array.length === 0){return 0} else {
+    array.sort((a,b) => (b-a));
+    return array[0];}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) { 
+ if (array === undefined){return 0}else if(array.length === 0){return 0}else{
+  var sum = array.reduce((a,b) => a+b,0);
+  return sum/array.length}
 }
